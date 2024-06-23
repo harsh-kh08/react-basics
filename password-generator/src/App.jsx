b5f7deb9ae3071547b359bdbc1c5c23ce484feb1
Harsh Khandelwal <harshkhandelwal08052000@gmail.com>
2024-06-23 11:59:18 +0530

    Radio buttons edited

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -206,8 +206,8 @@ Input type=chechbox -> to find whether the checkbox is checked or unchecked, use
       setNumber((prev)=>!prev); //In set function, we can pass function also
     }
   } id='numbers' type="checkbox" className="sr-only peer" defaultChecked={number} ></input>
-  <div className="flex flex-row justify-center items-center relative w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-  <span className="ms-3 text-xl font-medium text-gray-900 dark:text-gray-300">Numbers</span>
+  <div className="flex flex-row justify-center items-center relative w-14 h-7 bg-gray-900 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  <span className="ms-3 text-xl font-medium text-white dark:text-gray-900">Numbers</span>
 </label>
         </div>
 
@@ -229,8 +229,8 @@ Input type=chechbox -> to find whether the checkbox is checked or unchecked, use
       setCharacter((prev)=>!prev); //In set function, we can pass function also
     }
   } id='character' type="checkbox" className="sr-only peer"></input>
-  <div className="flex flex-row justify-center items-center relative w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-  <span className="ms-3 text-xl font-medium text-gray-900 dark:text-gray-300">Characters</span>
+  <div className="flex flex-row justify-center items-center relative w-14 h-7 bg-gray-900 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  <span className="ms-3 text-xl font-medium text-white dark:text-gray-900">Characters</span>
 </label>
         </div>
 
